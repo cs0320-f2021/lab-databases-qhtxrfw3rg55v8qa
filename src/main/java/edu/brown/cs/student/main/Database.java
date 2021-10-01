d3079de855a0155c6999df421e6cd8b5dc5511f0
Bella Antoniuk <84292736+bellantoniuk0@users.noreply.github.com>
2021-09-30 22:50:18 -0400

    completed databases lab

--- src/main/java/edu/brown/cs/student/main/Database.java
+++ src/main/java/edu/brown/cs/student/main/Database.java
@@ -1,9 +1,6 @@
 package edu.brown.cs.student.main;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -30,14 +27,39 @@ public class Database {
    */
   Database(String filename) throws SQLException, ClassNotFoundException {
 
-    /*
-     * TODO: Initialize the database connection, turn foreign keys on,
-     *  and then create the word and corpus tables if they do not exist.
-     */
+    try {
+      // this line loads the driver manager class, and must be
+      // present for everything else to work properly
+      Class.forName("org.sqlite.JDBC");
+      String urlToDB = "jdbc:sqlite:" + filename;
+      Connection conn = DriverManager.getConnection(urlToDB);
+      // these two lines tell the database to enforce foreign keys during operations, and should be present
+      Statement stat = conn.createStatement();
+      stat.close();
+
+      PreparedStatement prep;
+      prep = conn.prepareStatement("CREATE TABLE IF NOT EXISTS corpus("
+              + "(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+              + "filename TEXT;");
+      prep.executeUpdate();
+      prep.close();
+
+      prep = conn.prepareStatement("CREATE TABLE IF NOT EXISTS word"
+              + "(corpus_id INTEGER,"
+              + "word TEXT,"
+              + "FOREIGN KEY (corpus_id)"
+              + "REFERENCES corpus(id) ON DELETE CASCADE);");
+      prep.executeUpdate();
+      prep.close();
+
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    } catch (SQLException throwables) {
+      throwables.printStackTrace();
+    }
   }
 
-
-  /**
+    /**
    * Reads words from a text file if the file hasn't been already loaded.
    *
    * @param filename name of file to read.
@@ -117,8 +139,7 @@ public class Database {
    */
   Map<String, Integer> getFrequencyMap() throws SQLException {
     Map<String, Integer> freqMap = new HashMap<>();
-    //TODO: select all filenames and how many words are associated with those filenames from the database
-    PreparedStatement prep = conn.prepareStatement(""); //Your SQL here!
+    PreparedStatement prep = conn.prepareStatement("SELECT corpus.filename, COUNT(*) FROM corpus JOIN word ON corpus.id=word.corpus_id GROUP BY corpus.id;"); //Your SQL here!
     ResultSet rs = prep.executeQuery();
     while (rs.next()) {
       freqMap.put(rs.getString(1), rs.getInt(2));
@@ -139,8 +160,7 @@ public class Database {
    */
   Map<String, Integer> getInstanceMap() throws SQLException {
     Map<String, Integer> instMap = new HashMap<>();
-    //TODO: select the five most common words from the entire database, and how many times they appear
-    PreparedStatement prep = conn.prepareStatement(""); //Your SQL Here!
+    PreparedStatement prep = conn.prepareStatement("SELECT word, COUNT(*) FROM word GROUP BY word ORDER BY COUNT(*) DESC LIMIT 5;"); //Your SQL Here!
     ResultSet rs = prep.executeQuery();
     while (rs.next()) {
       instMap.put(rs.getString(1), rs.getInt(2));
